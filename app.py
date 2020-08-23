9544f267f5d8990951f074e5f7e0de6f4ffac57b
Michael Berry <66836804+michaelthomasberry@users.noreply.github.com>
2020-08-23 08:57:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    ## to change the port in flask you have to type: export FLASK_APP=application.py
+
